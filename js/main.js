0c53afe251201de69feb89dd1d810ccfe30c6390
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-06-07 03:29:45 -0300

    Layout funcional

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+(function ($) {
+	"use strict"; 
 
+	$(document).on("ready", function () {
+
+		$(".stagedLink").on("click", function (event) {
+
+			var offset, id, link = $(this).attr("href");
+
+			event.preventDefault();
+
+			id 		= "#" + link.substr(1);
+
+			if (id === "#") {
+				id 	= "#home";
+			}
+
+			// document.location.hash = id;
+
+			offset 	= $(id).offset();
+
+			$("html, body").animate({
+				"scrollTop": (offset.top - 40)
+			}, 200);
+
+		});
+
+	});
+
+}(jQuery));
