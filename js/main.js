daa1fe3665e91264e047d84094f79d88cdfa1c8a
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-06-10 17:19:10 -0300

    Active on scroll && pushState

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 (function ($) {
 	"use strict"; 
 
+	$.fn.makeActive = function () {
+		var title = $(this).children("a").attr("title"),
+			id = $(this).children("a").attr("href").substr(1);
+
+		$("nav ul li.active").removeClass("active");
+
+		if (id === "") {
+			id 	= "home";
+		}
+
+		History.pushState({fromScroll: true}, "Week - " + title, id);
+
+		return this.addClass("active");
+	};
+
 	$(document).on("ready", function () {
+
+		$(document).scrollTop(1).scrollTop(0);
 
 		$(".stagedLink").on("click", function (event) {
 
@@ -9,17 +26,11 @@
 
 			event.preventDefault();
 
-			$("nav ul li.active").removeClass("active");
-
-			$(this).parent("li").addClass("active");
-
 			id 		= "#" + link.substr(1);
 
 			if (id === "#") {
 				id 	= "#home";
 			}
-
-			// document.location.hash = id;
 
 			offset 	= $(id).offset();
 
@@ -28,6 +39,21 @@
 			}, 200);
 
 		});
+
+	});
+
+	$(document).on("scroll", function (event) {
+		var scrollTop = $(document).scrollTop();
+
+		if (scrollTop < $("#menu").offset().top - 41) {
+			$("nav ul li").eq(0).makeActive();
+		} else if (scrollTop <= $("#sobre-week").offset().top - 40) {
+			$("nav ul li").eq(1).makeActive();
+		} else if (scrollTop <= $("#contacto").offset().top - 40) {
+			$("nav ul li").eq(2).makeActive();
+		} else {
+			$("nav ul li").eq(3).makeActive();
+		}
 
 	});
 
